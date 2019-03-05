6f79127edad6c22669051c28bad56385add1fea9
Arata Kagan <aratakagan@gmail.com>
2019-03-04 19:15:23 -0800

    Added youtube modal and styling

--- client/src/components/TV/TV.js
+++ client/src/components/TV/TV.js
@@ -2,13 +2,16 @@ import React, {Component}  from 'react';
 import axios from 'axios';
 import Iframe from 'react-iframe';
 import './TV.css';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w200/';
 const API_KEY = 'fb6a1d3f38c3d97f67df6d141f936f29'
 
 class TV extends Component {
     state = {
         selectedTV: [],
-        youtube: []
+        youtubeKey: [],
+        showYouTubeModal: false
     }
 
     componentDidMount(){
@@ -21,32 +24,76 @@ class TV extends Component {
             });
         axios.get(`https://api.themoviedb.org/3/tv/${this.props.match.params.id}/videos?api_key=${API_KEY}&language=en-US`)
             .then(response => {
-                console.log('[TV Youtube]', response);
-                this.setState({
-                    youtube: response.data.results
-                })
-            })
+                console.log('[TV Youtube]', response.data.results[0].key);
+                response.data.results.map( result => this.setState({
+                    youtubeKey: 'http://www.youtube.com/embed/' + result.key
+                }));
+            });
+        }
+
+    youTubeModalShow(){
+        console.log('clicked');
+        this.setState({
+            showYouTubeModal: true
+        })   
+    }
+
+    youTubeModalClose(){
+        this.setState({
+            showYouTubeModal: false
+        })
+        
     }
 
     render(){
+        const networkName = [];
+        const genres = [];
         const tv = this.state.selectedTV;
-        console.log(tv);
-        const video = this.state.youtube[0];
-       
-       
+        console.log('youtube key:', this.state.youtubeKey);
+
+        const youtubeVideo = 
+            <div className='youtube_video'>
+            <Button variant="secondary" onClick={() => this.youTubeModalShow()}>Play Trailer</Button>
+                <Modal show={this.state.showYouTubeModal} onHide={() => this.youTubeModalClose()}>
+                    <Modal.Body>
+                        <Iframe url = {this.state.youtubeKey}
+                        width="450px"
+                        height="450px"
+                        id="myId"
+                        className="youTubeVideo"
+                        display="initial"
+                        position="relative"
+                        allowFullScreen />
+                    </Modal.Body>
+                </Modal>
+            </div>
+
+        for(var i in tv.networks){
+            networkName.push(tv.networks[i].logo_path)
+        }
+        for(var i in tv.genres){
+            genres.push(tv.genres[i].name + '/');
+        }
+        
         return(
-            <div className='container' key={tv.id}>
-                <a href={tv.homepage}><img id='tv_image' src={IMAGE_BASE_URL + tv.poster_path} alt={tv.name}/></a>
-                <h3 className='title'>{tv.name}</h3>
-                <p className='overview'>{tv.overview}</p>
-                {/* <Iframe url="http://www.youtube.com/embed/"
-                    width="450px"
-                    height="450px"
-                    id="myId"
-                    className="myClassname"
-                    display="initial"
-                    position="relative"
-                    allowFullScreen/> */}
+            <div className='tv_container'>
+                <div className='poster' key={tv.id}>
+                    <a href={tv.homepage}><img id='tv_image' src={IMAGE_BASE_URL + tv.poster_path} alt={tv.name}/></a>
+                </div>
+                <div className='poster_wrapper'>
+                    <div className='title_row'>
+                        <h3 className='title'>{tv.name}</h3>
+                        {youtubeVideo}
+                    </div>
+                    <div className='tv_info'>
+                        <p className='overview' dir='auto'>{tv.overview}</p>
+                        <img className='network_image' src={IMAGE_BASE_URL + networkName} alt={tv.name} />
+                        <p>First Air Date: {tv.first_air_date}</p>
+                        <p>Last Air Date: {tv.last_air_date}</p>
+                        <p>Number of Episodes: {tv.number_of_episodes} </p>
+                        <p>{genres}</p>
+                    </div>
+                </div>
             </div>
         )
     }
